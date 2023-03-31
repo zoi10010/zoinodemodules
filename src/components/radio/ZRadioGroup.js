be501bdb696d6da1ae36180192bfbfb9bbc1993c
Dharani <dharani.m@zoifintech.com>
2023-03-31 10:38:50 +0530

    design changes in form fields(text, radio, cb, ) by Dharani

--- src/components/radio/ZRadioGroup.js
+++ src/components/radio/ZRadioGroup.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
 export default function ZRadioGroup(props) {
 
     return (
-        <RadioGroup {...props}>
-            {props.children}
-        </RadioGroup>
+        <FormControlLabel
+            control={
+                <RadioGroup {...props}>
+                    {props.children}    
+                </RadioGroup>
+            }
+            label={props.name}
+         />
     );
 }
--- src/components/textField/ZTextBox.js
+++ src/components/textField/ZTextBox.js
@@ -10,6 +10,7 @@ function ZTextBox(props) {
     return (
         <FormControl className={classes.formControl}>
             <TextField
+                InputProps className={classes.inputIcon}
                 {...props}
                 
             />
@@ -17,13 +18,79 @@ function ZTextBox(props) {
     )
 }
 
-const styles = theme => ({
-    formControl: {
-        width: "100%"
+const useStyles = makeStyles(({  
+    formControl:{
+        width:"100%",
+        "& label":{
+            color:Colors.placeholder,
+            fontSize:constants.fontSize.sm,
+            "& + div.MuiInputBase-adornedEnd":{
+                paddingRight: ".65rem"
+            },
+
+            "& + div.MuiInputBase-root":{
+                "&:hover":{
+                    "& fieldset.MuiOutlinedInput-notchedOutline":{
+                        borderColor:Colors.highlightedBorder
+                    }
+                }              
+            },   
+            
+            "& + div.MuiInputBase-root.Mui-focused":{
+                "& fieldset.MuiOutlinedInput-notchedOutline":{
+                    border: `${constants.border.solid} ${Colors.highlightedBorder}`,                    
+                }       
+            },  
+        },
+        
+       
+
+        "& fieldset.MuiOutlinedInput-notchedOutline":{
+            borderColor:Colors.border
     },
-    textStyle: {
-        fontSize: 10
-    }
-});
+
+        "& label.MuiInputLabel-outlined[data-shrink='true']":{
+            transform: "translate(.875rem, -.5625rem) scale(1)",
+            fontSize:constants.fontSize.xs,
+            "& + .MuiInputBase-formControl":{
+                "& input.MuiOutlinedInput-input":{
+                    padding:".375rem .875rem"
+                }
+            }
+        },
+        
+       "& label.MuiInputLabel-outlined[data-shrink='false']":{
+            lineHeight: "0.85em",
+            transform: "translate(.75rem, .75rem) scale(1)",
+            fontSize:constants.fontSize.sm,
+            "& + .MuiInputBase-formControl":{
+                "& input.MuiOutlinedInput-input":{
+                    padding:".375rem .875rem"
+                }
+            }
+       },
+
+       "& label.MuiInputLabel-standard":{
+        "& + .MuiInputBase-root:before":{
+            borderBottom: `${constants.border.solid} ${Colors.border}`,
+        },
+        "& + .MuiInputBase-root:after":{
+            borderBottom: `${constants.border.solid} ${Colors.highlightedBorder}`,
+        }
+       },
+
+       "& p.MuiFormHelperText-root":{
+        color:Colors.red
+       }
+
+    },
+
+    inputIcon:{
+        "& div.MuiInputAdornment-root > span":{
+            display:constants.display.flex,
+            alignItems: constants.flexItems.center,
+        }
+      }
+  }));
 
 export default withStyles(styles)(ZTextBox);
