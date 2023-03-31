1d829ca5cb1744c10eb9ff7b73fddbf02697b7ea
melvinzoi <61777837+melvinzoi@users.noreply.github.com>
2023-03-31 16:43:31 +0530

    Revert "design changes in form fields(text, radio, cb, ) by Dharani"
    This reverts commit be501bdb696d6da1ae36180192bfbfb9bbc1993c.

--- src/components/radio/ZRadioGroup.js
+++ src/components/radio/ZRadioGroup.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
 
 export default function ZRadioGroup(props) {
 
     return (
-        <FormControlLabel
-            control={
-                <RadioGroup {...props}>
-                    {props.children}    
-                </RadioGroup>
-            }
-            label={props.name}
-         />
+        <RadioGroup {...props}>
+            {props.children}
+        </RadioGroup>
     );
 }
