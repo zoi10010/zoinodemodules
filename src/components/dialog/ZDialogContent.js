16903cff9b3256e1fe8a59a7ec70171ede9b0363
tamilarasi <tamilarasi.s@zoiftspl.com>
2023-04-30 19:57:51 +0530

    Changes made in dialog content

--- src/components/dialog/ZDialogContent.js
+++ src/components/dialog/ZDialogContent.js
@@ -3,6 +3,7 @@ import Dialog from '@mui/material/Dialog';
 import { DialogContent } from '@material-ui/core';
 
 function ZDialogContent(props) {
+    
     return (
         <DialogContent{...props}>
             {props.children}
